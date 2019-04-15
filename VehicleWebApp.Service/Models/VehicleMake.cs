2632ecde9df5870e8c4fd4762841c7425d59dde1
Marko Satlan <markosatlan@gmail.com>
2019-04-15 13:28:34 +0200

    Domain models created (Vehicle Make and Model)

---
+++ VehicleWebApp.Service/Models/VehicleMake.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+
+namespace VehicleWebApp.Service.Models
+{
+    public class VehicleMake
+    {
+        [Required, DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public Guid Id { get; set; }
+
+        [Required, MaxLength(20)]
+        public string Name { get; set; }
+
+        public string Abbreviation { get; set; }
+
+        public IList<VehicleModel> Models { get; set; }
+
+    }
+}
---
+++ VehicleWebApp.Service/Models/VehicleModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+
+namespace VehicleWebApp.Service.Models
+{
+    public class VehicleModel
+    {
+        [Required, DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public Guid Id { get; set; }
+
+        [Required, MaxLength(20)]
+        public string Name { get; set; }
+
+        public string Abbreviation { get; set; }
+
+        public VehicleMake Make { get; set; }
+
+        public Guid MakeId { get; set; }
+
+    }
+}
